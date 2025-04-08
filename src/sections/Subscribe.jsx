2ccf4e9745cccaa2f8184c44e7d117bf4c05e31f
justin-onyango <ojustin446@gmail.com>
2025-04-08 12:22:31 +0300

    Initial Commit

---
+++ src/sections/Subscribe.jsx
@@ -0,0 +1,7 @@
+const Subscribe = () => {
+  return (
+    <div className="w-full justify-centre font-montserrat text-coral-red align-middle text-8xl">Subscribe</div>
+  )
+}
+
+export default Subscribe
---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffers,
+    Footer,
+    CustomerReviews,
+    Subscribe,
+}
